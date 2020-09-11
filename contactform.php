025f454a200742911fb3c06c5998bbf95f9ce03f
ramahdandadan18 <ramadhanrsa18@gmail.com>
2020-09-11 23:13:30 +0700

    update contact form

--- contactform.php
+++ contactform.php
@@ -6,4 +6,6 @@ $recipient = "ramadhanrsa18@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+echo $email, $name;
+
+//echo "Thank You!";
